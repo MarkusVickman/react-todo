8bbaf40acc5c33b8ed4047c572218777a2111b03
Markus <markus.vickman@hotmail.com>
2025-02-04 12:31:40 +0100

    skapat en liten mall där moment 2 ska lösas i. saknar det mesta men jag en parent och en skild komponent samt interface men ingen data.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -5,7 +5,7 @@ import './Table.css'
 function Table({ todo }: { todo: TodoInterface }) {
 
     //Tar emot props och skriver ut värden på rätt plats med Vilkor för att bestäma stil och text som skrivs ut.
-    // Kan ändra om klar eller ej med funktionen changeIfCompleted och dess id
+    // Kan ändra om klar eller ej med funktionen changeIfCompleted och dess id.
     return (<article className="article" id={(todo.id).toString()}>
         <h2>{todo.title}</h2>
         <p><b>Beskrivning: </b>{todo.description}</p>
